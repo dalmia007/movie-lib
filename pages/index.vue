c1d27bc0862f9672553bb9ab4c86adc681122869
Anurag Dalmia <52538845+dalmia007@users.noreply.github.com>
2022-12-14 00:49:43 +0530

    feat: inital commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ data }}
+  </div>
+  <!-- <Tutorial /> -->
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      data: null,
+    }
+  },
+  mounted() {
+    this.getMovies()
+  },
+  methods: {
+    getMovies() {
+      this.$axios
+        .get(`https://6395ab63a68e43e418ee1a13.mockapi.io/movies`)
+        .then((res) => {
+          this.data = res.data
+        })
+        .catch((e) => {
+          console.error(e)
+        })
+    },
+  },
+})
+</script>
