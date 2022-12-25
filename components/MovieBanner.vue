073d79df295ff0c83c90700450d6e83ff53fa6ff
Anurag Dalmia <52538845+dalmia007@users.noreply.github.com>
2022-12-25 17:32:58 +0530

    feat:added nlink to banner

--- components/MovieBanner.vue
+++ components/MovieBanner.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="bg-white container mx-auto flex flex-wrap justify-center">
     <div v-for="movie in movies" :key="movie.id">
-      <div
-        class="rounded shadow-lg overflow-hidden m-2 transition duration-325 transform hover:-translate-y-1 hover:-translate-x-1 hover:scale-101 w-[300px] text-center"
-      >
-        <img :src="movie.poster" class="movie-image" />
-        <p>Release: {{ movie.releaseDate }}</p>
-        <p>Rating: {{ movie.rating }}</p>
-      </div>
+      <n-link :to="`/movie/${movie.id}`">
+        <div
+          class="rounded shadow-lg overflow-hidden m-2 transition duration-325 transform hover:-translate-y-1 hover:-translate-x-1 hover:scale-101 w-[300px] text-center"
+        >
+          <img :src="movie.poster" class="movie-image" />
+          <p>Release: {{ movie.releaseDate }}</p>
+          <p>Rating: {{ movie.rating }}</p>
+        </div>
+      </n-link>
     </div>
   </div>
 </template>
