4ff0228524d1a3a179c8ca2d20d4bfc64cf3c555
Anurag Dalmia <52538845+dalmia007@users.noreply.github.com>
2022-12-26 19:46:10 +0530

    feat: added unicon and seperated moviedetail component

--- pages/movie/_id.vue
+++ pages/movie/_id.vue
@@ -1,33 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center m-auto md:h-screen">
-    <div class="py-3 lg:max-w-[50%] sm:mx-auto">
-      <div
-        class="md:bg-white md:shadow-lg md:border-gray-100 md:border md:rounded-3xl p-8 flex space-x-8"
-      >
-        <div class="hidden md:block h-48 overflow-visible w-1/2">
-          <img class="rounded-3xl shadow-lg" :src="data.poster" alt="" />
-        </div>
-        <div class="flex flex-col md:w-1/2 space-y-4">
-          <div class="flex md:hidden h-72 overflow-visible w-3/4 mx-auto">
-            <img class="rounded-3xl shadow-lg" :src="data.poster" alt="" />
-          </div>
-          <div class="flex justify-between items-start">
-            <h2 class="text-2xl font-semibold">{{ data.title }}</h2>
-          </div>
-          <div>
-            <div class="bg-yellow-400 max-w-max rounded-lg p-2 mb-1">
-              {{ data.rating }}
-            </div>
-            <div class="text-lg mt-1">{{ data.director }}</div>
-            <div class="text-lg">{{ data.releaseDate }}</div>
-          </div>
-          <p class="text-gray-400 md:max-h-40 overflow-y-scroll">
-            {{ data.synopsis }}
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <MovieDetail :data="data" />
 </template>
 
 <script lang="ts">
---
+++ plugins/vue-unicons.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Unicon from 'vue-unicons/dist/vue-unicons-vue2.umd'
+import { uniPen } from 'vue-unicons/dist/icons'
+
+Unicon.add([uniPen])
+Vue.use(Unicon)
